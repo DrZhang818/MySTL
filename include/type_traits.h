8130d85560da7c4de763af089fca85db9154edae
DrZhang818 <3143565781@qq.com>
2026-02-17 20:50:25 +0800

    update

--- include/type_traits.h
+++ include/type_traits.h
@@ -30,20 +30,20 @@ template <typename T>
 using remove_reference_t = typename remove_reference<T>::type;
 
 template <typename T> struct remove_const           { using type = T; };
-template <typename T> struct remove_const<T const>  { using type = T; };
+template <typename T> struct remove_const<const T>  { using type = T; };
 
 template <typename T>
 using remove_const_t = typename remove_const<T>::type;
 
 template <typename T> struct remove_volatile              { using type = T; };
-template <typename T> struct remove_volatile<T volatile>  { using type = T; };
+template <typename T> struct remove_volatile<volatile T>  { using type = T; };
 
 template <typename T>
 using remove_volatile_t = typename remove_volatile<T>::type;
 
 template <typename T>
 struct remove_cv {
-    using type = typename remove_volatile_t<typename remove_const_T<T>::type>::type;
+    using type = typename remove_volatile<typename remove_const<T>::type>::type;
 };
 
 template <typename T>
@@ -78,14 +78,10 @@ struct is_void : bool_constant<is_void_v<T>> {};
 
 template <typename T>
 struct is_trivially_destructible
-    : bool_constant<__is_trivially_destructible(T)> {};
+    : bool_constant<__has_trivial_destructor(T)> {};
 
 template <typename T>
-inline constexpr bool is_trivially_destructible_v = is_trivially_destrctible<T>::value;
-
-template <typename T, T v>
-using m_integral_constant = std::integral_constant<T, v>;
-
+inline constexpr bool is_trivially_destructible_v = is_trivially_destructible<T>::value;
 
 template <typename T1, typename T2>
 struct pair;
@@ -94,10 +90,10 @@ template <typename T>
 struct is_pair : false_type {};
 
 template <typename T1, typename T2>
-struct is_pair<mystl::pair<T1, T2>> : true_type {};
+struct is_pair<pair<T1, T2>> : true_type {};
 
 template <typename T>
-inline constexpr bool is_pair_v = is_pair<std::remove_cv_t<T>>::value;
+inline constexpr bool is_pair_v = is_pair<remove_cv_t<T>>::value;
 
 template <typename T>
 concept Pair = is_pair_v<T>;
---
+++ test/test_type_traits.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include "type_traits.h"
+
+namespace mystl {
+    template <typename T1, typename T2>
+    struct pair {
+        T1 first;
+        T2 second;
+    };
+}
+
+struct TrivialStruct {
+    int x;
+    float y;
+};
+
+struct NonTrivialStruct {
+    int* ptr;
+    NonTrivialStruct() { ptr = new int(0); }
+    ~NonTrivialStruct() { delete ptr; }
+};
+
+void ref_check(int&) {
+    std::cout << "Lvalue reference check Passed!" << std::endl;
+}
+
+void ref_check(int&&) {
+    std::cout << "Rvalue reference check Passed!" << std::endl;
+}
+
+template <typename T>
+void wrapper(T&& arg) {
+    ref_check(mystl::forward<T>(arg));
+}
+
+int main() {
+    std::cout << "Starting type traits tests..." << std::endl;
+
+    static_assert(mystl::true_type::value == true, "true_type test failed");
+    static_assert(mystl::false_type::value == false, "false_type test failed");
+
+    static_assert(mystl::is_same_v<int, int> == true, "is_same<int, int> test failed");
+    static_assert(mystl::is_same_v<int, float> == false, "is_same<int, float> test failed");
+
+    std::cout << "Basic Helpers (true_type, false_type, is_same) tests passed!" << std::endl;
+
+    static_assert(mystl::is_same_v<mystl::remove_reference_t<int>,int>, "remove_reference<int> test failed");
+    static_assert(mystl::is_same_v<mystl::remove_reference_t<int&>, int>, "remove_reference<int&> test failed");
+    static_assert(mystl::is_same_v<mystl::remove_reference_t<int&&>, int>, "remove_reference<int&&> test failed");
+
+    static_assert(mystl::is_same_v<mystl::remove_const_t<const int>, int>, "remove_const<const int> test failed");
+    static_assert(mystl::is_same_v<mystl::remove_volatile_t<volatile int>, int>, "remove_volatile<volatile int> test failed");
+
+    static_assert(mystl::is_same_v<mystl::remove_cv_t<const volatile int>, int>, "remove_cv<const volatile int> test failed");
+
+    std::cout << "Type Modifications (remove_reference, remove_const, remove_volatile, remove_cv) tests passed!" << std::endl;
+
+    static_assert(mystl::is_pair_v<mystl::pair<int, double>> == true, "is_pair<pair<int, double>> test failed");
+    static_assert(mystl::is_pair_v<int> == false, "is_pair<int> negative test failed");
+
+    static_assert(mystl::is_pair_v<const mystl::pair<int, int>> == true, "is_pair<const pair<int, int>> test failed");
+
+    std::cout << "is_pair tests passed!" << std::endl;
+
+    static_assert(mystl::is_trivially_destructible_v<int> == true, "int should be trivial");
+    static_assert(mystl::is_trivially_destructible_v<TrivialStruct> == true, "Strcut should be trivial");
+    static_assert(mystl::is_trivially_destructible_v<NonTrivialStruct> == false, "Class with destructor should NOT be trivial");
+
+    std::cout << "Compiler Intrinsics (is_trivially_destructible) tests passed!" << std::endl;
+
+    std::cout << "Testing forward and move semantics..." << std::endl;
+    int x = 42;
+    wrapper(x);
+    wrapper(42);
+    wrapper(mystl::move(x));
+
+    static_assert(mystl::is_same_v<decltype(mystl::move(x)), int&&>, "move return type test failed");
+
+    std::cout << "\nAll Compile-Time Assertions Passed!" << std::endl;
+    
+    return 0;
+}
